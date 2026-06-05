439b21639a4378cefe35cb92e80664fc34c84151
wafaaidan <wafa.aidan@gmail.com>
2026-06-05 16:01:06 +0700

    membuat abstract class dan class orang dgn getter

--- orang.java
+++ orang.java
@@ -1,4 +1,14 @@
-public class orang {
+abstract class Orang {
+    private String nama;
+
+    public Orang(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
     
     public static void main(String[] args) {
         
