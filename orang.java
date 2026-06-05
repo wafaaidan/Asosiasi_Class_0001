49d2382f350ca8d1ccee91ac4d79ab4bf945894b
wafaaidan <wafa.aidan@gmail.com>
2026-06-05 16:04:57 +0700

    class matakuliah

--- orang.java
+++ orang.java
@@ -10,7 +10,17 @@ abstract class Orang {
     }
 
 public abstract void tampilkanPeran();
-    
+class MataKuliah {
+    private String kode;
+    private String nama;
+    private int sks;
+
+    public MataKuliah(String kode, String nama, int sks) {
+        this.kode = kode;
+        this.nama = nama;
+        this.sks = sks;
+    }
+}
     public static void main(String[] args) {
         
     }
