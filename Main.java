ab9b5338553f14cd27969d6843a8604c28059572
wafaaidan <wafa.aidan@gmail.com>
2026-06-19 16:43:29 +0700

    membuat class main & method viewmataKuliahDiajar

---
+++ Main.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+
+abstract class orang {
+    private String nama;
+
+    public orang(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public abstract void tampilkanPeran();
+}
+class MataKuliah {
+    private String kode;
+    private String nama;
+    private int sks;
+
+    public MataKuliah(String kode, String nama, int sks) {
+        this.kode = kode;
+        this.nama = nama;
+        this.sks = sks;
+    }
+
+    public String getInfo(){
+        return kode + " - " + nama + "(" + sks + "SKS)";
+    }
+}
+class Dosen extends orang {
+    private String nidn;
+    private ArrayList<MataKuliah> MataKuliahDiajar;
+
+    public Dosen(String nama, String nidn) {
+        super(nama);
+        this.nidn = nidn;
+        this.MataKuliahDiajar = new ArrayList<>();
+    }
+
+    public void tambahMataKuliah(MataKuliah mataKuliah){
+        MataKuliahDiajar.add(mataKuliah);
+    }
+    @Override
+    public void tampilkanPeran(){
+        System.out.println(getNama() + "");
+    }
+    public void tampilkanMataKuliahDiajar(){
+        System.out.println("Dosen : " + getNama());
+        System.out.println("NIDN :" + nidn);
+        System.out.println("Mata Kuliah yang Diajar");
+
+        for (MataKuliah mk : MataKuliahDiajar) {
+            System.out.println("-" + mk.getInfo());
+
+        }
+    }
+}
+public class Main {
+    
+}
+
